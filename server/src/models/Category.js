952aa369e93893dc280821436352b6c374e4752a
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-01-19 16:06:44 +0700

    feat: add categoryModel

--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
+// src/models/Category.js
+import mongoose from 'mongoose'; // Ubah require menjadi import
 
-const categorySchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    description: {
-      type: String,
-    },
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
   },
-  { timestamps: true },
-);
+  description: {
+    type: String
+  }
+}, { timestamps: true });
 
-export default categorySchema;
+// Ubah module.exports menjadi export default
+export default mongoose.model('Category', categorySchema);
