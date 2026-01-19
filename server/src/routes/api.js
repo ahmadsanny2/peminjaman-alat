ca85211e75f9151abed04efd672e2f3e8cd0323e
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-01-19 16:08:40 +0700

    feat: add routes tools and categories api

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,9 +1,19 @@
 import express from "express";
 import authController from "../controllers/authController.js";
+import toolController from "../controllers/toolController.js";
+import categoryController from "../controllers/categoryController.js";
 
 const router = express.Router();
 
+// Auth
 router.post("/auth/register", authController.register);
 router.post("/auth/login", authController.login);
+
+// Tool
+router.post("/admin/dashboard/tools/add-tool", toolController.create);
+router.get("/admin/dashboard/tools", toolController.getAll);
+
+// Category
+router.post("/admin/dashboard/categories/add-category", categoryController.create);
 
 export default router;
