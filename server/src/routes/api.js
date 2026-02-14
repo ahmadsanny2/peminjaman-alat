44db4c28d1b95a12a8f7fb7ae42a462105a08f56
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-15 00:49:15 +0700

    chore: add routes api register and login users

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,48 +1,9 @@
-import express from "express";
-import authController from "../controllers/authController.js";
-import toolController from "../controllers/toolController.js";
-import categoryController from "../controllers/categoryController.js";
-import { checkRole, verifyToken } from "../middleware/authMiddleware.js";
-import loanController from "../controllers/loanController.js";
+import express from 'express'
+import authController from '../controllers/authController.js'
 
-const router = express.Router();
+const router = express.Router()
 
-// Auth
-router.post("/auth/register", authController.register);
-router.post("/auth/login", authController.login);
-router.use(verifyToken);
+router.post('/auth/register', authController.register)
+router.post('/auth/login', authController.login)
 
-// Tool
-router.get("/admin/dashboard/tools", toolController.getAll);
-router.post("/admin/dashboard/tools/add-tool", toolController.create);
-
-// Category
-router.get("/admin/dashboard/categories", categoryController.getAll);
-router.post(
-    "/admin/dashboard/categories/add-category",
-    categoryController.create,
-);
-
-router.post(
-    "/loans/request",
-    checkRole(["peminjam"]),
-    loanController.createLoan,
-);
-router.get("/loans/my-loans", loanController.getMyLoans);
-router.put(
-    "/loans/:id/approve",
-    checkRole(["petugas", "admin"]),
-    loanController.approveLoan,
-);
-router.put(
-    "/loans/:id/return",
-    checkRole(["petugas", "admin"]),
-    loanController.returnLoan,
-);
-router.get(
-    "/loans",
-    checkRole(["petugas", "admin"]),
-    loanController.getAllLoans,
-);
-
-export default router;
+export default router
