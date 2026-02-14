ef27d91bf641d463e10188b90ed2b0ffb8bc404d
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-14 18:24:00 +0700

    feat: add middleware

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,35 +1,31 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken'
 
 export const verifyToken = (req, res, next) => {
-    const authHeader = req.header("Authorization");
-    const token = authHeader && authHeader.split(" ")[1];
+    const authHeader = req.header('Authorization')
+    const token = authHeader && authHeader.split(' ')[1]
 
-    if (!token) {
-        return res.status(401).json({
-            message: "Akses ditolak! Token tidak ditemukan.",
-        });
-    }
+    if (!token) return res.status(401).json({
+        message: "Akses ditolak. Token autentikasi tidak ditemukan."
+    })
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = decoded;
-
-        next();
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
     } catch (error) {
         res.status(403).json({
-            message: "Token tidak valid atau kadaluarsa.",
-        });
+            message: "Integritas token tidak valid atau sesi telah kedaluwarsa."
+        })
     }
-};
+}
 
 export const checkRole = (allowedRoles) => {
     return (req, res, next) => {
         if (!allowedRoles.includes(req.user.role)) {
             return res.status(403).json({
-                message: "Anda tidak memiliki izin untuk mengakses fitur ini.",
-            });
+                message: "Pelanggaran hak akses. Anda tidak memiliki otorisasi untuk fitur ini."
+            })
         }
-        next();
-    };
-};
+        next()
+    }
+}
