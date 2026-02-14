66e65fdc3a4e9c511b86486be2bd5117774955a5
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-14 20:40:31 +0700

    feat: add table relation

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,21 +1,21 @@
-import sequelize from '../config/db.js';
-import User from './User.js';
-import Category from './Category.js';
-import Tool from './Tool.js';
-import Loan from './Loan.js';
+import sequelize from "../config/db.js";
+import User from "./User.js";
+import Category from "./Category.js";
+import Tool from "./Tool.js";
+import Loan from "./Loan.js";
 
 // Relasi Kategori dan Alat (One-to-Many)
-Category.hasMany(Tool, { foreignKey: 'categoryId' });
-Tool.belongsTo(Category, { foreignKey: 'categoryId' });
+Category.hasMany(Tool, { foreignKey: "categoryId" });
+Tool.belongsTo(Category, { foreignKey: "categoryId" });
 
 // Relasi Peminjaman dengan Alat dan Pengguna
-User.hasMany(Loan, { foreignKey: 'borrowerId', as: 'borrowings' });
-Loan.belongsTo(User, { foreignKey: 'borrowerId', as: 'borrower' });
+User.hasMany(Loan, { foreignKey: "borrowerId", as: "borrowings" });
+Loan.belongsTo(User, { foreignKey: "borrowerId", as: "borrower" });
 
-User.hasMany(Loan, { foreignKey: 'officerId', as: 'approvals' });
-Loan.belongsTo(User, { foreignKey: 'officerId', as: 'officer' });
+User.hasMany(Loan, { foreignKey: "officerId", as: "approvals" });
+Loan.belongsTo(User, { foreignKey: "officerId", as: "officer" });
 
-Tool.hasMany(Loan, { foreignKey: 'toolId' });
-Loan.belongsTo(Tool, { foreignKey: 'toolId' });
+Tool.hasMany(Loan, { foreignKey: "toolId" });
+Loan.belongsTo(Tool, { foreignKey: "toolId" });
 
 export { sequelize, User, Category, Tool, Loan };
