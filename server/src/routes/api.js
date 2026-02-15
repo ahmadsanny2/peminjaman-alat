fd427f61e9561c17b84ae32ddc0cd2e27628d9ba
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-16 05:25:33 +0700

    chore: add routes api getAllCategories

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,9 +1,18 @@
-import express from 'express'
-import authController from '../controllers/authController.js'
+import express from "express";
+import authController from "../controllers/authController.js";
+import categoryController from "../controllers/categoryController.js";
+// import { checkRole } from '../middleware/roleMiddleware.js'
+import { verifyToken, checkRole } from "../middleware/authMiddleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/auth/register', authController.register)
-router.post('/auth/login', authController.login)
+// Authentication
+router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
 
-export default router
+router.use(verifyToken);
+
+// Categories
+router.get("/categories", categoryController.getAllCategories);
+
+export default router;
