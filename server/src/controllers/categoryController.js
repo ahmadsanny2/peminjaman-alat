9a58c065785c0ccda79106b3a4011b0a4d1ee6f0
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-16 02:49:50 +0700

    refactor: tidy up the code using mysql and sequelize for deleteCategory controller

--- server/src/controllers/categoryController.js
+++ server/src/controllers/categoryController.js
@@ -70,5 +70,28 @@ export default {
         }
     },
 
-    
+    async deleteCategory(req, res) {
+        try {
+            const { id } = req.params
+
+            const category = await Category.findByPk(id)
+
+            if (!category) {
+                return res.status(404).json({
+                    message: "Category not found"
+                })
+            }
+
+            await category.destroy()
+
+            res.status(200).json({
+                message: "Category deleted successfully"
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: "An error occurred while deleting the category",
+                error: error.message
+            })
+        }
+    }
 }
