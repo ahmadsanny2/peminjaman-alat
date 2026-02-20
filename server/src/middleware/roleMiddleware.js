ed97e13d2e653116a6e7a6c6b7fd95f07ecd28cc
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-20 21:44:55 +0700

    refactor: tidy up the code

---
+++ server/src/middleware/roleMiddleware.js
@@ -0,0 +1,10 @@
+export const checkRole = (allowedRoles) => {
+    return (req, res, next) => {
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({
+                message: "Forbidden. You don't have permission to access this resource."
+            })
+        }
+        next()
+    }
+}
