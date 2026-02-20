340f49622b4b3dad7e8ac89d98cf5901da7a378e
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-20 21:48:21 +0700

    chore: add routes api auth

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,33 +1,19 @@
 import express from "express";
+
 import authController from "../controllers/authController.js";
 import categoryController from "../controllers/categoryController.js";
-import { checkRole } from '../middleware/roleMiddleware.js'
+import toolController from "../controllers/toolController.js";
+import loanController from "../controllers/loanController.js";
+import userController from "../controllers/userController.js";
+
 import { verifyToken } from "../middleware/authMiddleware.js";
+import { checkRole } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
-// Authentication
 router.post("/auth/register", authController.register);
 router.post("/auth/login", authController.login);
 
 router.use(verifyToken);
-
-// Categories
-router.get("/categories", categoryController.getAllCategories);
-router.post(
-    "/categories/add-category",
-    checkRole(["admin"]),
-    categoryController.createCategory,
-);
-router.put(
-    "/categories/update-category/:id",
-    checkRole(["admin"]),
-    categoryController.updateCategory,
-);
-router.delete(
-    "/categories/delete-category/:id",
-    checkRole(["admin"]),
-    categoryController.deleteCategory,
-);
 
 export default router;
