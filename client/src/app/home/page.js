464cf7763c65e5f705f6d29787da612ea7bef552
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-25 03:56:34 +0700

    refactor: tidy up the code

---
+++ client/src/app/home/page.js
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar/page";
+import Hero from "./hero-section/page";
+
+const HomePage = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default HomePage;
