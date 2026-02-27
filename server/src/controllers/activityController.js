901724e0f1e2c9b47ecbf7249ed9b5a16e146907
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-27 17:39:26 +0700

    feat: make log activiy controller

---
+++ server/src/controllers/activityController.js
@@ -0,0 +1,21 @@
+import { ActivityLog, User } from "../models/index.js";
+
+export default {
+    async getLogs(req, res) {
+        try {
+            const logs = await ActivityLog.findAll({
+                include: [
+                    { model: User, as: "actor", attributes: ["fullName", "role"] },
+                ],
+                order: [["createdAt", "DESC"]],
+                limit: 100,
+            });
+            res.status(200).json({ data: logs });
+        } catch (error) {
+            res.status(500).json({
+                message: "Kegagalan ekstraksi rekam jejak sistem.",
+                error: error.message,
+            });
+        }
+    },
+};
