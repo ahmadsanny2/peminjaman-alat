dbc2f34115c60da98d8378c9136edfa2ec260fd6
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-02-28 17:13:40 +0700

    feat: make utils recordActivity

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -1,20 +1,21 @@
-import { ActivityLog } from '../models/index.js';
+import { ActivityLog } from "../models/index.js";
 
 /**
- * Utilitas sentral untuk merekam jejak aktivitas operasional.
- * * @param {string} userId 
- * @param {string} action 
- * @param {string} description 
+ * * @param {string} userId
+ * @param {string} action
+ * @param {string} description
  */
 export const recordActivity = async (userId, action, description) => {
     try {
         await ActivityLog.create({
             userId,
             action,
-            description
+            description,
         });
     } catch (error) {
-
-        console.error('There is a discrepancy in the structure of the logging device.', error.message);
+        console.error(
+            "There is a discrepancy in the structure of the logging device.",
+            error.message,
+        );
     }
 };
