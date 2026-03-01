99e8dfebec3c3e6e51ac42fb7d92958625df858f
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-01 12:06:37 +0700

    refactor: remove content type headers

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -3,9 +3,6 @@ import Cookies from "js-cookie";
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
 
 api.interceptors.request.use(
