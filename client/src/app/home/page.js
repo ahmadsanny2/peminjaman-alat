5788d71611d25b3d407aaf70b27c6a96ee77b385
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-04 00:20:34 +0700

    feat: add UI landing page

--- client/src/app/home/page.js
+++ client/src/app/home/page.js
@@ -1,11 +1,15 @@
 import Navbar from "@/components/Navbar/page";
 import Hero from "./hero-section/page";
+import Why from "./why-section/page";
+import Feature from "./main-feature/page";
 
 const HomePage = () => {
   return (
     <div className="">
       <Navbar />
       <Hero />
+      <Why />
+      <Feature />
     </div>
   );
 };
