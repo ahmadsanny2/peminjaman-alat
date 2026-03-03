b5bb656bc6a434154c563f5dc6cfb0f5015771ab
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-03 19:39:36 +0700

    feat: add pagination

--- client/src/app/(dashboard)/admin/management-categories/page.js
+++ client/src/app/(dashboard)/admin/management-categories/page.js
@@ -1,12 +1,20 @@
 "use client";
 
 import { useCategory } from "@/hooks/admin/useCategoryManagement";
-import { Edit, Trash2, Plus, X, Tags, Edit2 } from "lucide-react";
+import { Edit, Trash2, Plus, X, Tags, Edit2, ChevronRight, ChevronLeft } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function CategoryManagementPage() {
 
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const page = Number(searchParams.get("page")) || 1;
+
+    // Category Management Data
     const {
         categories,
+        totalPages,
         formData,
         isEditing,
         isSubmitting,
@@ -16,10 +24,11 @@ export default function CategoryManagementPage() {
         handleEdit,
         handleDelete,
         resetForm,
-    } = useCategory();
+    } = useCategory(page, 5);
 
     return (
         <div className="space-y-6">
+
             {/* Header */}
             <div className="flex items-center gap-3 border-b border-slate-200 pb-4">
                 <Tags className="text-emerald-600" size={32} />
@@ -28,15 +37,19 @@ export default function CategoryManagementPage() {
                 </div>
             </div>
 
+            {/* Error Response */}
             {error && (
                 <div className="bg-red-50 text-red-700 p-3 rounded border border-red-200 text-sm">
                     {error}
                 </div>
             )}
 
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-                {/* Form */}
-                <div className="lg:col-span-1">
+            {/* Main Content */}
+            <div className="flex flex-col gap-2">
+
+                <div className="flex flex-col gap-2">
+
+                    {/* Form */}
                     <div className="bg-white p-6 rounded-xl border border-slate-200 shadow-sm sticky top-6">
                         <h2 className="text-lg font-semibold text-slate-800 mb-4 flex items-center gap-2">
                             {isEditing ? (
@@ -101,18 +114,18 @@ export default function CategoryManagementPage() {
                                         <X size={20} />
                                     </button>
                                 )}
+
                             </div>
                         </form>
                     </div>
-                </div>
 
-                {/* Tabel Kategori */}
-                <div className="lg:col-span-2">
-                    <div className="bg-white rounded-xl border border-slate-200 shadow-sm overflow-hidden">
+                    {/* Table Data Category */}
+                    <div className="bg-white rounded-xl border border-slate-200 shadow-sm overflow-hidden h-90">
                         <div className="overflow-x-auto">
                             <table className="w-full text-left text-sm text-slate-600">
                                 <thead className="bg-slate-50 border-b border-slate-200 text-slate-800 font-semibold">
                                     <tr>
+
                                         <th className="px-6 py-4">Nama Kategori</th>
                                         <th className="px-6 py-4">Deskripsi</th>
                                         <th className="px-6 py-4 text-center">Aksi</th>
@@ -156,8 +169,34 @@ export default function CategoryManagementPage() {
                             </table>
                         </div>
                     </div>
+
                 </div>
+
+                {/* Pagination */}
+                <div className="flex items-center justify-between px-6 py-3 border border-slate-800 rounded-2xl">
+                    <span className="text-xs text-slate-500 font-medium">
+                        Halaman {page} dari {totalPages}
+                    </span>
+                    <div className="flex gap-2">
+                        <button
+                            disabled={page === 1}
+                            onClick={() => router.push(`?page=${page - 1}`)}
+                            className="p-1.5 rounded bg-white border border-slate-300 text-slate-600 hover:bg-slate-100 disabled:opacity-50 transition-colors cursor-pointer disabled:cursor-default"
+                        >
+                            <ChevronLeft size={16} />
+                        </button>
+                        <button
+                            disabled={page === totalPages}
+                            onClick={() => router.push(`?page=${page + 1}`)}
+                            className="p-1.5 rounded bg-white border border-slate-300 text-slate-600 hover:bg-slate-100 disabled:opacity-50 transition-colors cursor-pointer disabled:cursor-default"
+                        >
+                            <ChevronRight size={16} />
+                        </button>
+                    </div>
+                </div>
+
             </div>
+
         </div>
     );
 }
