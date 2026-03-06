5ccd89e1e640153f5b066dd31f970211df407907
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-06 19:03:05 +0700

    feat: make useActivity hooks

--- client/src/hooks/admin/useActivityLog.js
+++ client/src/hooks/admin/useActivityLog.js
@@ -1,27 +1,35 @@
 import api from "@/lib/api";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-export const useActivityLog = () => {
+export const useActivityLog = (page = 1, limit = 10) => {
     const [logs, setLogs] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState("");
 
-    // Fetch Data From Server
-    useEffect(() => {
-        const fetchLog = async () => {
-            setIsLoading(true);
-            try {
-                const response = await api.get("/logs");
-                setLogs(response.data.data);
-                setError("");
-            } catch (err) {
-                setError("Gagal memuat log aktivitas dari server.");
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        fetchLog();
-    }, []);
 
-    return { logs, isLoading, error }
+    const [totalPages, setTotalPages] = useState(1);
+    const [totalItems, setTotalItems] = useState(0);
+
+    // Fetch Data From Server
+    const fetchLog = useCallback(async () => {
+        setIsLoading(true);
+        try {
+            const response = await api.get(`/logs?page=${page}&limit=${limit}`);
+
+            setLogs(response.data.data);
+            setTotalPages(response.data.totalPages || 1);
+            setTotalItems(response.data.totalItems);
+            setError("");
+        } catch (err) {
+            setError("Gagal memuat log aktivitas dari server.");
+        } finally {
+            setIsLoading(false);
+        }
+    }, [page, limit]);
+
+    useEffect(() => {
+        fetchLog();
+    }, [fetchLog]);
+
+    return { logs, isLoading, error, totalItems, totalPages }
 };
