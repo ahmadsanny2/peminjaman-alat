22ce31150434137fabeac0d902b59e5fcfb38d47
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-08 02:03:46 +0700

    feat: add filterAndSearch Data

--- client/src/app/(dashboard)/admin/management-categories/page.js
+++ client/src/app/(dashboard)/admin/management-categories/page.js
@@ -2,6 +2,7 @@
 
 import FilterAndSearchData from "@/components/FilterAndSearchData";
 import Modal from "@/components/Modal";
+import Pagination from "@/components/Pagination";
 import { useCategory } from "@/hooks/admin/useCategoryManagement";
 import {
     Edit,
@@ -10,14 +11,11 @@ import {
     X,
     Tags,
     Edit2,
-    ChevronRight,
-    ChevronLeft,
 } from "lucide-react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export default function CategoryManagementPage() {
     const searchParams = useSearchParams();
-    const router = useRouter();
 
     const page = Number(searchParams.get("page")) || 1;
 
@@ -38,33 +36,37 @@ export default function CategoryManagementPage() {
         setSearch,
         handleShowForm,
         showForm,
+        totalItems
     } = useCategory(page, 10);
 
     return (
-        <div className="space-y-6">
-            {/* Header */}
-            <div className="flex items-center gap-3 border-b border-slate-200 pb-4">
-                <Tags className="text-emerald-600" size={32} />
-                <div>
-                    <h1 className="text-2xl font-bold">Manajemen Kategori</h1>
+        <div className="flex flex-col justify-between h-full space-y-6">
+
+            <div className="space-y-6">
+
+                {/* Header */}
+                <div className="flex items-center gap-3 border-b border-slate-200 pb-4">
+                    <Tags className="text-emerald-600" size={32} />
+                    <div>
+                        <h1 className="text-2xl font-bold">Manajemen Kategori</h1>
+                    </div>
                 </div>
-            </div>
 
-            {/* Error Response */}
-            {error && (
-                <div className="bg-red-50 text-red-700 p-3 rounded border border-red-200 text-sm">
-                    {error}
-                </div>
-            )}
+                {/* Error Response */}
+                {error && (
+                    <div className="bg-red-50 text-red-700 p-3 rounded border border-red-200 text-sm">
+                        {error}
+                    </div>
+                )}
 
-            {/* Main Content */}
-            <div className="flex flex-col gap-5">
-                {/* Filter and Search Category */}
-                <FilterAndSearchData search={(e) => setSearch(e.target.value)} sort={(e) => setSort(e.target.value)} isShowForm={handleShowForm} placeHolderName="Cari nama kategori..." />
+                {/* Content */}
+                <div className="space-y-6">
 
-                <div className="flex flex-col gap-2">
+                    {/* Filter and Search Category */}
+                    <FilterAndSearchData search={(e) => setSearch(e.target.value)} sort={(e) => setSort(e.target.value)} isShowForm={handleShowForm} placeHolderName="Cari nama kategori..." />
+
                     {/* Form */}
-                    <Modal customClass={showForm ? 'fixed inset-0 flex items-center justify-center' : 'hidden'} isOpen={handleShowForm} onClose={handleShowForm}>
+                    <Modal customClass={showForm ? 'fixed inset-0 h-full flex items-center justify-center' : 'hidden'} isOpen={handleShowForm} onClose={handleShowForm}>
                         <h2 className="text-lg font-semibold text-slate-800 mb-4 flex items-center gap-2">
                             {isEditing ? (
                                 <>
@@ -133,7 +135,7 @@ export default function CategoryManagementPage() {
                     </Modal>
 
                     {/* Table Data Category */}
-                    <div className="bg-white rounded-xl border border-slate-200 shadow-sm min-h-175">
+                    <div className="bg-white rounded-xl border border-slate-200 shadow-sm">
                         <div className="overflow-x-auto rounded-xl">
                             <table className="w-full text-sm text-slate-600">
                                 <thead className="bg-slate-50 border-b border-slate-200 text-slate-800 font-semibold">
@@ -169,7 +171,7 @@ export default function CategoryManagementPage() {
                                                 <td className="px-6 py-4 truncate max-w-xs">
                                                     {category.description || "-"}
                                                 </td>
-                                                <td className="px-6 py-4 text-center">
+                                                <td className="px-6 py-4 text-center min-w-30">
                                                     <button
                                                         onClick={() => handleEdit(category)}
                                                         className="p-1 text-blue-600 bg-blue-50 rounded-lg cursor-pointer"
@@ -192,31 +194,14 @@ export default function CategoryManagementPage() {
                             </table>
                         </div>
                     </div>
+
                 </div>
 
-                {/* Pagination */}
-                <div className="flex items-center justify-between px-6 py-3 border border-slate-800 rounded-2xl">
-                    <span className="text-xs text-slate-500 font-medium">
-                        Halaman {page} dari {totalPages}
-                    </span>
-                    <div className="flex gap-2">
-                        <button
-                            disabled={page === 1}
-                            onClick={() => router.push(`?page=${page - 1}`)}
-                            className="p-1.5 rounded bg-white border border-slate-300 text-slate-600 hover:bg-slate-100 disabled:opacity-50 transition-colors cursor-pointer disabled:cursor-default"
-                        >
-                            <ChevronLeft size={16} />
-                        </button>
-                        <button
-                            disabled={page === totalPages}
-                            onClick={() => router.push(`?page=${page + 1}`)}
-                            className="p-1.5 rounded bg-white border border-slate-300 text-slate-600 hover:bg-slate-100 disabled:opacity-50 transition-colors cursor-pointer disabled:cursor-default"
-                        >
-                            <ChevronRight size={16} />
-                        </button>
-                    </div>
-                </div>
             </div>
+
+            {/* Pagination */}
+            <Pagination page={page} totalPages={totalPages} totalData={totalItems} />
+
         </div>
     );
 }
