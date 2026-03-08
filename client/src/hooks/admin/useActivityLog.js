b6b4a6e8353ffbbf894774dbb86db18c67170217
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-09 00:46:10 +0700

    feat: add filterAndSearch Data hooks

--- client/src/hooks/admin/useActivityLog.js
+++ client/src/hooks/admin/useActivityLog.js
@@ -10,26 +10,29 @@ export const useActivityLog = (page = 1, limit = 10) => {
     const [totalPages, setTotalPages] = useState(1);
     const [totalItems, setTotalItems] = useState(0);
 
+    const [sort, setSort] = useState("")
+    const [showByActivity, setShowByActivity] = useState("")
+
     // Fetch Data From Server
     const fetchLog = useCallback(async () => {
         setIsLoading(true);
         try {
-            const response = await api.get(`/logs?page=${page}&limit=${limit}`);
+            const response = await api.get(`/logs`, { params: { sort, activity: showByActivity, page, limit } });
 
             setLogs(response.data.data);
             setTotalPages(response.data.totalPages || 1);
             setTotalItems(response.data.totalItems);
             setError("");
         } catch (err) {
-            setError("Gagal memuat log aktivitas dari server.");
+            setError("Gagal mengambil data log aktivitas dari server.");
         } finally {
             setIsLoading(false);
         }
-    }, [page, limit]);
+    }, [sort, showByActivity, page, limit]);
 
     useEffect(() => {
         fetchLog();
     }, [fetchLog]);
 
-    return { logs, isLoading, error, totalItems, totalPages }
+    return { logs, isLoading, error, totalItems, totalPages, setSort, setShowByActivity }
 };
