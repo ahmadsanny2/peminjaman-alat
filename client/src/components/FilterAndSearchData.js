89da8d9e51510399a8c607a44b905081c724e24f
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-09 00:46:45 +0700

    refactor: tidy up the code

--- client/src/components/FilterAndSearchData.js
+++ client/src/components/FilterAndSearchData.js
@@ -1,23 +1,36 @@
+import { useActivityLog } from "@/hooks/admin/useActivityLog";
 import { useTool } from "@/hooks/admin/useToolManagement";
+import { useUserManagement } from "@/hooks/admin/useUserManagement";
 import { Filter, Search, Plus } from "lucide-react";
 
+export default function FilterAndSearchData({
+    search,
+    sort,
+    isShowForm,
+    placeHolderName,
+    isCategory,
+    showByCategory,
+    hidden,
+    isRole,
+    showByRole,
+    showByActivity,
+    isLogActivity
+}) {
 
-export default function FilterAndSearchData({ search, sort, isShowForm, placeHolderName, isCategory, showByCategory }) {
-    const { categories } = useTool()
+    const { categories } = useTool();
+    const { users } = useUserManagement()
+    const { logs } = useActivityLog()
 
     return (
-        <form className="flex flex-col md:flex-row justify-between gap-2 z-0">
-            <div className="flex bg-white/15 items-center backdrop-blur-2xl rounded-2xl">
+        <form className="bg-white/15 backdrop-blur-2xl rounded-2xl p-5 z-0 space-y-6">
+            <div className="flex items-center gap-2">
+                <Filter />
+                <h1 className="text-xl font-semibold">Filter</h1>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
 
-                <div className="px-3 py-2">
-                    <Filter />
-                </div>
-
-                <div className="border-l border-l-slate-600 px-3 py-2">
-                    <select
-                        className="outline-none cursor-pointer"
-                        onChange={sort}
-                    >
+                <div className="flex items-center bg-white/15 backdrop-blur-2xl p-2 gap-2 rounded-2xl max-md:col-span-2">
+                    <select className="outline-none cursor-pointer w-full" onChange={sort}>
                         <option className="bg-white/20 text-black" value="">
                             Urutkan
                         </option>
@@ -30,50 +43,95 @@ export default function FilterAndSearchData({ search, sort, isShowForm, placeHol
                     </select>
                 </div>
 
-
                 {isCategory ? (
-                    <div className="border-l border-l-slate-600 px-3 py-2 w-full">
+                    <div className="flex items-center bg-white/15 backdrop-blur-2xl p-2 gap-2 rounded-2xl max-md:col-span-2">
                         <select
                             className="outline-none cursor-pointer w-full"
                             onChange={showByCategory}
                         >
-                            <option className="bg-white/20 text-black" value="" disabled>
-                                Kategori
-                            </option>
                             <option className="bg-white/20 text-black" value="">
                                 Semua
                             </option>
                             {categories.map((cat) => (
-                                <option className="bg-white/20 text-black" value={cat.name} key={cat.id}>
+                                <option
+                                    className="bg-white/20 text-black"
+                                    value={cat.name}
+                                    key={cat.id}
+                                >
                                     {cat.name}
                                 </option>
                             ))}
                         </select>
                     </div>
+                ) : null}
 
-                ) : (
-                    null
-                )}
-            </div>
+                {isRole ? (
+                    <div className="flex items-center bg-white/15 backdrop-blur-2xl p-2 gap-2 rounded-2xl max-md:col-span-2">
+                        <select
+                            className="outline-none cursor-pointer w-full"
+                            onChange={showByRole}
+                        >
+                            <option className="bg-white/20 text-black" value="">
+                                Semua
+                            </option>
+                            {users.map((user) => (
+                                <option
+                                    className="bg-white/20 text-black"
+                                    value={user.role}
+                                    key={user.id}
+                                >
+                                    {user.role.charAt(0).toUpperCase() + user.role.slice(1)}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                ) : null}
 
-            <div className="flex">
-                <div className="flex items-center bg-white/15 backdrop-blur-2xl p-2 gap-2 rounded-l-2xl w-full">
-                    <Search />
-                    <input
-                        type="text"
-                        className="outline-none focus:border-b w-full"
-                        onChange={search}
-                        placeholder={placeHolderName}
-                    />
-                </div>
+                {isLogActivity ? (
+                    <div className="flex items-center bg-white/15 backdrop-blur-2xl p-2 gap-2 rounded-2xl max-md:col-span-2">
+                        <select
+                            className="outline-none cursor-pointer w-full"
+                            onChange={showByActivity}
+                        >
+                            <option className="bg-white/20 text-black" value="">
+                                Semua
+                            </option>
+                            {logs.map((log, index) => (
+                                <option
+                                    className="bg-white/20 text-black"
+                                    value={log.action}
+                                    key={index}
+                                >
+                                    {log.action}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                ) : null}
 
-                <div
-                    className="bg-blue-500 p-2 cursor-pointer max-w-fit rounded-r-2xl"
-                    onClick={isShowForm}
-                >
-                    <Plus />
+                <div className={`flex ${(isCategory || isRole || isLogActivity) ? "col-span-2" : ""} max-md:col-span-2`}>
+                    <div className={`flex items-center bg-white/15 backdrop-blur-2xl p-2 gap-2 ${!hidden ? "rounded-l-2xl" : "rounded-2xl"} w-full`}>
+                        <Search />
+                        <input
+                            type="text"
+                            className="outline-none focus:border-b w-full"
+                            onChange={search}
+                            placeholder={placeHolderName}
+                        />
+                    </div>
+
+
+                    {!hidden ? (
+                        <div
+                            className="bg-blue-500 p-2 cursor-pointer max-w-fit rounded-r-2xl"
+                            onClick={isShowForm}
+                        >
+                            <Plus />
+                        </div>
+                    ) : null}
+
                 </div>
             </div>
         </form>
-    )
+    );
 }
