1e7c93aecad64c1d14373c6796472e385f1f51f3
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-16 18:53:14 +0700

    refactor: rename Modal.js to modalComponent.js

--- client/src/components/ModalComponent.js
+++ client/src/components/ModalComponent.js
@@ -2,7 +2,7 @@ export default function Modal({ isOpen, children, customClass }) {
     if (!isOpen) return null;
 
     return (
-        <div className={`bg-black/50 h-full z-10 ${customClass}`}>
+        <div className={`bg-black/50 h-full z-10 ${customClass} p-5`}>
             <div className="bg-white text-black rounded-lg shadow-lg w-150 p-6 relative">
                 {children}
             </div>
