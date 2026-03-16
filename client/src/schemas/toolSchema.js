6c52767f68ca95c1ec4746c99bd28ed9b30eecad
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-17 01:59:54 +0700

    refactor: update toolSchema

--- client/src/schemas/toolSchema.js
+++ client/src/schemas/toolSchema.js
@@ -1,17 +1,24 @@
 import { z } from "zod";
 
 export const toolSchema = z.object({
-    name: z.string().min(3, {
-        message: "Tool name must be at least 3 characters.",
-    }),
-    categoryId: z.string().uuid({
-        message: "Invalid category ID.",
-    }),
-    stock: z.preprocess(
-        (val) => Number(val),
-        z.number().min(0, {
-            message: "Stock must be at least 0.",
-        }),
-    ),
-    // image: z.string().optional,
+    name: z.string().min(3, "Nama alat minimal 3 karakter"),
+    description: z.string().min(10, "Deskripsi minimal 10 karakter"),
+
+    condition: z.union([
+        z.enum(["Bagus", "Rusak"]),
+        z.literal("").refine(() => false, { message: "Pilih kondisi alat" })
+    ]),
+
+    categoryId: z
+        .string()
+        .min(1, "Silakan pilih kategori alat")
+        .uuid("Format kategori tidak valid"),
+
+    stock: z.any()
+        .refine((val) => val !== "", { message: "Stok harus diisi" })
+        .transform((val) => Number(val))
+        .refine((val) => !isNaN(val), { message: "Stok harus berupa angka" })
+        .pipe(z.number().min(0, "Stok minimal 0")),
+
+    image: z.any().optional()
 });
