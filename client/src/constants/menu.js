66b1eb5f80cc5ac0f05d76505118fb6e30a6e564
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-23 09:44:06 +0700

    chore: add constants menu

---
+++ client/src/constants/menu.js
@@ -0,0 +1,22 @@
+export const menu = [
+    {
+        name: "Home",
+        url: "/"
+    },
+    {
+        name: "Fitur",
+        url: "#feature"
+    },
+    {
+        name: "Role Pengguna",
+        url: "#role"
+    },
+    {
+        name: "Cara Kerja",
+        url: "/"
+    },
+    {
+        name: "Tentang",
+        url: "#about"
+    }
+]
