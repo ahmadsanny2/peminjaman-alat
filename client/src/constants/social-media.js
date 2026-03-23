6ccabaffacb020ce6cdf773440bbb367e302508c
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-23 09:44:22 +0700

    chore: add constants social-media

---
+++ client/src/constants/social-media.js
@@ -0,0 +1,19 @@
+import { Instagram ,Linkedin,Github} from "lucide-react";
+
+export const socialMedia = [
+    {
+        name: "Github",
+        url: "https://www.github.com/ahmadsanny2/",
+        icon: <Github />
+    },
+    {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/ahmadsanny02/",
+        icon: <Linkedin />
+    },
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/ahmadsanny02/",
+        icon: <Instagram />
+    },
+]
