37db1b97c3cbf0c8cb7229ed91f917908e7b22a2
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-27 18:51:25 +0700

    refactor: rename file

---
+++ server/src/models/user.model.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("admin", "petugas", "peminjam"),
+      allowNull: false,
+    },
+  },
+  { tableName: "users" },
+);
+
+export default User;
