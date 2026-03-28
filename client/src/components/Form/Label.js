f3e0e34aea3f65cf6ea77bbb8f1fabce30df960a
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-29 02:54:11 +0700

    feat: make label component

---
+++ client/src/components/Form/Label.js
@@ -0,0 +1,9 @@
+const Label = ({ name }) => {
+    return (
+        <label className="block text-sm font-medium text-slate-700 mb-1">
+            {name}
+        </label>
+    )
+}
+
+export default Label
