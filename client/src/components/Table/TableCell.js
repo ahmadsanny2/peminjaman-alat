fa168730b22c6d55596eb2d03930e69d641cc141
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-30 18:02:06 +0700

    feat: make Table Cell component

---
+++ client/src/components/Table/TableCell.js
@@ -0,0 +1,13 @@
+const TableCell = ({ isHeader, children, className, colspan }) => {
+    return isHeader ? (
+        <th className={`border-slate-200 px-2 py-4 border ${className}`}>{children}</th>
+    ) : (
+        <td className={`border-slate-200 px-2 py-4 border ${className}`}
+            colSpan={colspan}
+        >
+            {children}
+        </td>
+    );
+};
+
+export default TableCell;
