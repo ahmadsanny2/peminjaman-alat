232ee5e44c9c0a8459c9869423a10ffd420cd13e
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-04-01 20:06:47 +0700

    feat: add feat verifying return loan

--- client/src/app/(dashboard)/admin/loan-transactions/page.js
+++ client/src/app/(dashboard)/admin/loan-transactions/page.js
@@ -1,14 +1,22 @@
 "use client";
 
-import { AlertCircle, CheckCircle, ClipboardList, Undo2, XCircle } from "lucide-react";
+import {
+    AlertCircle,
+    CheckCircle,
+    ClipboardList,
+    Undo2,
+    XCircle,
+} from "lucide-react";
 
 import FilterAndSearchData from "@/components/FilterAndSearchData";
 import Pagination from "@/components/Pagination";
 import StatusBadge from "@/components/StatusBadge";
 import { useLoanManagement } from "@/hooks/admin/useLoanManagement";
+import { Check } from "lucide-react";
+import ActionButton from "@/components/ActionButton";
+import { ClipboardCheck } from "lucide-react";
 
 export default function LoanManagementPage() {
-
     // Loan Management Data
     const {
         loans,
@@ -17,20 +25,19 @@ export default function LoanManagementPage() {
         error,
         approveLoan,
         rejectLoan,
-        returnLoan,
+        verifying,
         page,
         totalPages,
         totalItems,
         limit,
         updateFilters,
         handleSearch,
-        formatDateTime
+        formatDateTime,
     } = useLoanManagement();
 
     return (
         <div className="flex flex-col justify-between h-full space-y-6">
             <div className="space-y-6">
-
                 {/* Header */}
                 <div className="flex items-center gap-3 border-b border-slate-200 pb-4">
                     <ClipboardList className="text-yellow-600" size={32} />
@@ -50,25 +57,38 @@ export default function LoanManagementPage() {
                 <FilterAndSearchData
                     hiddenSearchData={!false}
                     placeHolderName="Cari nama peminjam..."
-                    sort={(e) => updateFilters('sort', e.target.value)}
+                    sort={(e) => updateFilters("sort", e.target.value)}
                     search={(e) => handleSearch(e.target.value)}
                 />
 
                 {/* Main Content */}
                 <div className="bg-white rounded-xl border border-slate-200 shadow-sm overflow-hidden">
                     <div className="overflow-x-auto">
-
                         {/* Table Data Loan Transaction History */}
                         <table className="w-full text-left text-sm text-slate-600">
                             <thead className="bg-slate-50 border-b border-slate-200 text-slate-800 font-semibold">
                                 <tr>
-                                    <th className="px-6 py-4 border border-slate-200 text-center">No</th>
-                                    <th className="px-6 py-4 border border-slate-200">Peminjam</th>
-                                    <th className="px-6 py-4 border border-slate-200">Spesifikasi Alat</th>
-                                    <th className="px-6 py-4 border border-slate-200 text-center">Tanggal Pengajuan</th>
-                                    <th className="px-6 py-4 border border-slate-200 text-center">Proyeksi Pengembalian</th>
-                                    <th className="px-6 py-4 border border-slate-200 text-center">Status Peminjaman</th>
-                                    <th className="px-6 py-4 border border-slate-200 text-center">Aksi</th>
+                                    <th className="px-6 py-4 border border-slate-200 text-center">
+                                        No
+                                    </th>
+                                    <th className="px-6 py-4 border border-slate-200">
+                                        Peminjam
+                                    </th>
+                                    <th className="px-6 py-4 border border-slate-200">
+                                        Spesifikasi Alat
+                                    </th>
+                                    <th className="px-6 py-4 border border-slate-200 text-center">
+                                        Tanggal Pengajuan
+                                    </th>
+                                    <th className="px-6 py-4 border border-slate-200 text-center">
+                                        Proyeksi Pengembalian
+                                    </th>
+                                    <th className="px-6 py-4 border border-slate-200 text-center">
+                                        Status Peminjaman
+                                    </th>
+                                    <th className="px-6 py-4 border border-slate-200 text-center">
+                                        Aksi
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody className="divide-y divide-slate-100">
@@ -92,7 +112,7 @@ export default function LoanManagementPage() {
                                     </tr>
                                 ) : (
                                     loans.map((loan, index) => {
-                                        const no = index + 1 + (page - 1) * limit
+                                        const no = index + 1 + (page - 1) * limit;
                                         return (
                                             <tr
                                                 key={loan.id}
@@ -101,7 +121,8 @@ export default function LoanManagementPage() {
                                                 <td className="text-center">{no}</td>
                                                 <td className="px-6 py-4">
                                                     <div className="font-medium text-slate-800 min-w-40">
-                                                        {loan.borrower?.fullName || "Identitas tidak dikenal"}
+                                                        {loan.borrower?.fullName ||
+                                                            "Identitas tidak dikenal"}
                                                     </div>
                                                     <div className="text-xs text-slate-400">
                                                         {loan.borrower?.username}
@@ -110,59 +131,48 @@ export default function LoanManagementPage() {
                                                 <td className="px-6 py-4 font-medium text-slate-800 truncate max-w-sm">
                                                     {loan.Tool?.name || "Nama alat tidak ada."}
                                                 </td>
-                                                <td className="px-6 py-4 text-center min-w-60">{formatDateTime(loan.borrowDate, false)}</td>
+                                                <td className="px-6 py-4 text-center min-w-60">
+                                                    {formatDateTime(loan.borrowDate, false)}
+                                                </td>
                                                 <td className="px-6 py-4 text-center min-w-60">
                                                     {formatDateTime(loan.expectedReturnDate, false)}
                                                 </td>
                                                 <td className="px-6 py-4 text-center min-w-50">
                                                     <StatusBadge status={loan.status} />
                                                 </td>
-                                                <td className="px-6 py-4 text-center space-x-2 min-w-30">
-                                                    {loan.status === "pending" && (
-                                                        <div className="flex items-center justify-center gap-2">
-                                                            <button
-                                                                onClick={() => approveLoan(loan.id)}
-                                                                disabled={isProcessing}
-                                                                className="text-emerald-600 rounded transition-colors text-xs font-medium disabled:opacity-50 cursor-pointer disabled:cursor-default"
-                                                                title="Setujui"
-                                                            >
-                                                                <CheckCircle size={14} />
-                                                            </button>
+                                                <td className="px-6 py-4 text-center">
+                                                    <div className="flex flex-col gap-2">
+                                                        <ActionButton
+                                                            disabled={loan.status !== "pending" || isProcessing}
+                                                            onClick={() => approveLoan(loan.id)}
+                                                            name="Setujui"
+                                                            color="emerald"
+                                                            icon={<CheckCircle size={16} />}
+                                                        />
 
-                                                            <button
-                                                                onClick={() => rejectLoan(loan.id)}
-                                                                disabled={isProcessing}
-                                                                className="text-red-600 rounded transition-colors text-xs font-medium disabled:opacity-50 cursor-pointer disabled:cursor-default"
-                                                                title="Tolak"
-                                                            >
-                                                                <XCircle size={14} />
-                                                            </button>
-                                                        </div>
-                                                    )}
+                                                        <ActionButton
+                                                            disabled={loan.status !== "pending" || isProcessing}
+                                                            onClick={() => rejectLoan(loan.id)}
+                                                            name="Tolak"
+                                                            color="rose"
+                                                            icon={<XCircle size={16} />}
+                                                        />
 
-                                                    {loan.status === "approved" && (
-                                                        <button
-                                                            onClick={() => returnLoan(loan.id)}
-                                                            disabled={isProcessing}
-                                                            className="text-yellow-600 transition-colors text-xs font-medium disabled:opacity-50 cursor-pointer disabled:cursor-default"
-                                                        >
-                                                            <Undo2 size={14} />
-                                                        </button>
-                                                    )}
-
-                                                    {(loan.status === "returned" || loan.status === "rejected" || loan.status === "canceled") && (
-                                                        <span className="text-xs font-semibold text-slate-400 italic">
-                                                            Selesai
-                                                        </span>
-                                                    )}
+                                                        <ActionButton
+                                                            disabled={loan.status !== "verifying" || isProcessing}
+                                                            onClick={() => verifying(loan.id)}
+                                                            name="Verifikasi"
+                                                            color="indigo"
+                                                            icon={<ClipboardCheck size={16} />}
+                                                        />
+                                                    </div>
                                                 </td>
                                             </tr>
-                                        )
+                                        );
                                     })
                                 )}
                             </tbody>
                         </table>
-
                     </div>
                 </div>
             </div>
