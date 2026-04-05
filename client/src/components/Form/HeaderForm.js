5e2e8c89a392c086ac47d59a973c826194ef81d9
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-04-05 11:49:16 +0700

    feat: add props className

--- client/src/components/Form/HeaderForm.js
+++ client/src/components/Form/HeaderForm.js
@@ -1,6 +1,6 @@
-const HeaderForm = ({ icon, title }) => {
+const HeaderForm = ({ icon, title, className }) => {
     return (
-        <div className="flex items-center gap-2 mb-4">
+        <div className={`flex items-center gap-2 mb-4 ${className}`}>
             {icon}
             <h2 className="text-lg font-semibold text-slate-800  flex items-center gap-2">
                 {title}
