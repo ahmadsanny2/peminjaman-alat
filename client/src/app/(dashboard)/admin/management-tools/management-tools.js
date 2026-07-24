95eab304510ef2b4b095623a2da32ba5470bda0f
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-25 05:51:34 +0700

    style(client): simplify layout container classes in management tools

--- client/src/app/(dashboard)/admin/management-tools/management-tools.js
+++ client/src/app/(dashboard)/admin/management-tools/management-tools.js
@@ -150,7 +150,7 @@ export default function ToolManagementContent() {
     }
 
     return (
-        <div className="flex flex-col justify-between h-full space-y-6">
+        <div className="space-y-6">
             <div className="space-y-6">
                 {/* Header */}
                 <HeaderPage
