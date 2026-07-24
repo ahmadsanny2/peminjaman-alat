fa1ff09422998e1d62ee893aa9a4920bffb77b56
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-25 05:51:40 +0700

    style(client): simplify layout container classes in transactions history

--- client/src/app/(dashboard)/borrower/transactions-history/transactions-history.js
+++ client/src/app/(dashboard)/borrower/transactions-history/transactions-history.js
@@ -192,7 +192,7 @@ export default function LoanTransactionHistoryContent() {
     }
 
     return (
-        <div className="flex flex-col justify-between h-full space-y-6">
+        <div className="space-y-6">
             <div className="space-y-6">
                 {/* Header */}
                 <HeaderPage
