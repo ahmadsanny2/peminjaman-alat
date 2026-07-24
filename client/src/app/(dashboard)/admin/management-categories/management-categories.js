17b957ed990fcad25aaeb2c45e2b603c1828f118
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-25 05:51:32 +0700

    style(client): simplify layout container classes in management categories

--- client/src/app/(dashboard)/admin/management-categories/management-categories.js
+++ client/src/app/(dashboard)/admin/management-categories/management-categories.js
@@ -133,7 +133,7 @@ export default function CategoryManagementContent() {
     }
 
     return (
-        <div className="flex flex-col justify-between h-full space-y-6">
+        <div className="space-y-6">
             <div className="space-y-6">
                 {/* Header */}
                 <HeaderPage
