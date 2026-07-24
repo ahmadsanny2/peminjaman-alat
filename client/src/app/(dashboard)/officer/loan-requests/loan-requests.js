fe5bea4eac12d9b46f1bfb21a395da15cc39ff6e
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-25 05:51:41 +0700

    style(client): simplify layout container classes in loan requests

--- client/src/app/(dashboard)/officer/loan-requests/loan-requests.js
+++ client/src/app/(dashboard)/officer/loan-requests/loan-requests.js
@@ -209,7 +209,7 @@ export default function LoanRequestsContent() {
     }
 
     return (
-        <div className="flex flex-col justify-between h-full space-y-6">
+        <div className="space-y-6">
             <div className="space-y-6">
                 {/* Header */}
                 <HeaderPage
