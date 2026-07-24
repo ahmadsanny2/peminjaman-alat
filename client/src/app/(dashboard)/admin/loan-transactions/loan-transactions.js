20c75ea3fff376d8fd23efc321c43837b44c00cf
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-25 05:51:31 +0700

    style(client): simplify layout container classes in loan transactions

--- client/src/app/(dashboard)/admin/loan-transactions/loan-transactions.js
+++ client/src/app/(dashboard)/admin/loan-transactions/loan-transactions.js
@@ -116,7 +116,7 @@ export default function LoanManagementContent() {
     }
 
     return (
-        <div className="flex flex-col justify-between h-full space-y-6">
+        <div className="space-y-6">
             <div className="space-y-6">
                 {/* Header */}
                 <HeaderPage
