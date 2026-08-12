71543efa2068d37c1e1ea5ea1971846f2aef2309
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-08-12 19:41:04 +0700

    fix(logger): support transactions in activity logging

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -5,13 +5,13 @@ import { ActivityLog } from "../models/index.js";
  * @param {string} action
  * @param {string} description
  */
-export const recordActivity = async (userId, action, description) => {
+export const recordActivity = async (userId, action, description, transaction) => {
     try {
         await ActivityLog.create({
             userId,
             action,
             description,
-        });
+        }, { transaction });
     } catch (error) {
         console.error(
             "There is a discrepancy in the structure of the logging device.",
